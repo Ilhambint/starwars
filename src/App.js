b35588e3dc0585d7ccb0fc3c1d680a5315ef59c5
Ilhambint <ilhambintangp7@gmail.com>
2022-04-10 00:24:37 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-function App() {
+import Home from "./Components/Pages/Home"
+
+
+import './main.css'
+
+import Header from "./Components/Header/Header"
+import Movie from "./Components/Pages/Movie"
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container">
+      
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie/:id" element={ <Movie/>}/>
+      </Routes>
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
 export default App;
